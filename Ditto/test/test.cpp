6d099de6369e736cd598737b4a7287ae2d93cd27
Robert Crawford <crawfordr4@nku.edu>
2015-03-19 13:17:26 -0400

    Committing...

--- Ditto/test/test.cpp
+++ Ditto/test/test.cpp
@@ -1,9 +1,19 @@
-#include "compiler.h"
+#include "src/compiler/compiler.h"
 
 int main(){
 	__ditto_compiler::compTools testTools;
-	__ditto_compiler::staticMemPool myMem;
-	myMem.setup(1024);
+
+    //Test the static memory code.
+	__ditto_compiler::staticMemPool staticMem;
+    staticMem.setup(1024);
+
+    //Set up test data...
+    char testChar = 'a';
+    char* testString = "Meet me in the library at 10pm.";
+    
+    //Unsigned
+    short testShort[] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    int testInt[]
     
 	return 1;
 }
