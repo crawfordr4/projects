f129ae7cfa7f217c93195f7d1c9f5cc72ed27fe3
Robert Crawford <crawfordr4@nku.edu>
2015-03-19 13:26:38 -0400

    Added more test cases.

--- Ditto/test/test.cpp
+++ Ditto/test/test.cpp
@@ -1,9 +1,9 @@
-#include "src/compiler/compiler.h"
+#include <iostream>
+#include <testZone/dataPool.cpp>
 
 int main(){
-	__ditto_compiler::compTools testTools;
-
-    //Test the static memory code.
+    
+    //Test cases for static memory code.
 	__ditto_compiler::staticMemPool staticMem;
     staticMem.setup(1024);
 
@@ -11,9 +11,21 @@ int main(){
     char testChar = 'a';
     char* testString = "Meet me in the library at 10pm.";
     
+    //Signed
+    short testShort[] = {0,1,2,-3,4,5,6,7,8,-9,10,11,12};
+    int testInt[] = {345,12,-3456,847573,2,0,-13567};
+    long testLong[] = {48563245,-49684986,2344566};
+
     //Unsigned
-    short testShort[] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
-    int testInt[]
+    unsigned short testUShort[] = {0,1,2,3,4,5,6,7,8,9};
+    unsigned int testUInt[] = {345,12,3456,847573,2,0,13567};
+    unsigned long testULong[] = {48563245,49684986,2344566};
+    
+    //Allocate items...
+    long charLocation = allocateChar(1,1,&testChar);
+    long stringLocation = allocateString(50,31,testString);
+    long shortLocation = allocateShort(13,13,testShort);
+    long intLocation = allocateInt(7,7,testInt);
     
 	return 1;
 }
